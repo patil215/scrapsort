693c2dcf2db52f598de7321f2d15585eb194ffe8
Kevin Chen <kevinbc@gmail.com>
2017-07-29 15:02:00 -0700

    clean up vision api

--- vision/vision.py
+++ vision/vision.py
@@ -1,30 +1,24 @@
-import io
-
 import base64
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
-import json
-import argparse
+
 
 def get_image_labels(file_path):
-	credentials = GoogleCredentials.get_application_default()
-	service = discovery.build('vision', 'v1', credentials=credentials)
+    credentials = GoogleCredentials.get_application_default()
+    service = discovery.build('vision', 'v1', credentials=credentials)
 
-	with open(file_path, 'rb') as image:
-		image_content = base64.b64encode(image.read())
-		service_request = service.images().annotate(body={
-			'requests': [{
-				'image': {
-					'content': image_content.decode('UTF-8')
-				},
-				'features': [{
-					'type': 'LABEL_DETECTION',
-					'maxResults': 10
-				}]
-			}]
-		})
-		response = service_request.execute()
-		print json.dumps(response, indent=4, sort_keys=True)
-
-
-print get_image_labels("bottle.jpg")
+    with open(file_path, 'rb') as image:
+        image_content = base64.b64encode(image.read())
+        service_request = service.images().annotate(body={
+            'requests': [{
+                'image': {
+                    'content': image_content.decode('UTF-8')
+                },
+                'features': [{
+                    'type': 'LABEL_DETECTION',
+                    'maxResults': 10
+                }]
+            }]
+        })
+        response = service_request.execute()
+        return response['responses'][0]['labelAnnotations']
