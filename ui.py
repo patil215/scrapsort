5019f3c4814ef71ab1d5d7b4cabdc5fbf2c3edaa
Filip <filip_bystricky@brown.edu>
2017-07-29 15:33:16 -0700

    added srolling messages

--- ui.py
+++ ui.py
@@ -1,21 +1,26 @@
 import rainbowhat
+import time
 
 def display_message(message):
     rainbowhat.display.print_str(message)
     rainbowhat.display.show()
+
+def display_long_message(message, delay=0.25):
+	for i in range(len(message) / 4):
+		display_message(message[i : min(i + 4, len(message))])
 
 def set_all_pixels(*args, **kwargs):
 	for pixel in range(7):
 		rainbowhat.rainbow.set_pixel(pixel, *args, **kwargs)
 	rainbowhat.rainbow.show()
 
-def show_decision(isTrash):
-	if isTrash:
+def show_decision(is_trash):
+	if is_trash:
 		set_all_pixels(1, 0, 0)
-		display_message("T")
+		display_long_message("TRASH")
 	else:
 		set_all_pixels(0, 1, 0)
-		display_message("R")
+		display_long_message("RECYCLING")
 
 @rainbowhat.touch.A.press()
 def press_a(channel):
