b675acddd9b1ea87b8a7168360e1db18d47b59bf
patil215 <neilpatil215@gmail.com>
2017-07-30 02:47:16 -0700

    Make classifier use tensorflow trained graph instead of local vision API

--- retraining-example.py
+++ retraining-example.py
@@ -1,38 +1,3 @@
-"""
-Preparing model:
- - Install bazel ( check tensorflow's github for more info )
-    Ubuntu 14.04:
-        - Requirements:
-            sudo add-apt-repository ppa:webupd8team/java
-            sudo apt-get update
-            sudo apt-get install oracle-java8-installer
-        - Download bazel, ( https://github.com/bazelbuild/bazel/releases )
-          tested on: https://github.com/bazelbuild/bazel/releases/download/0.2.0/bazel-0.2.0-jdk7-installer-linux-x86_64.sh
-        - chmod +x PATH_TO_INSTALL.SH
-        - ./PATH_TO_INSTALL.SH --user
-        - Place bazel onto path ( exact path to store shown in the output)
-- For retraining, prepare folder structure as
-    - root_folder_name
-        - class 1
-            - file1
-            - file2
-        - class 2
-            - file1
-            - file2
-- Clone tensorflow
-- Go to root of tensorflow
-- bazel build tensorflow/examples/image_retraining:retrain
-- bazel-bin/tensorflow/examples/image_retraining/retrain --image_dir /path/to/root_folder_name  --output_graph /path/output_graph.pb --output_labels /path/output_labels.txt --bottleneck_dir /path/bottleneck
-** Training done. **
-For testing through bazel,
-    bazel build tensorflow/examples/label_image:label_image && \
-    bazel-bin/tensorflow/examples/label_image/label_image \
-    --graph=/path/output_graph.pb --labels=/path/output_labels.txt \
-    --output_layer=final_result \
-    --image=/path/to/test/image
-For testing through python, change and run this code.
-"""
-
 import numpy as np
 import tensorflow as tf
 
--- vision.py
+++ vision.py
@@ -1,26 +1,50 @@
-import base64
-from googleapiclient import discovery
-from oauth2client.client import GoogleCredentials
+import os
+import numpy as np
+import tensorflow as tf
 
+class Classifier:
+    modelPath = None
+    labelPath = None
+    sess = None
+    softmax_tensor = None
 
-def get_image_labels(file_path):
-    credentials = GoogleCredentials.get_application_default()
-    service = discovery.build('vision', 'v1', credentials=credentials)
+    def __init__(self, modelPath, labelPath):
+        self.modelPath = modelPath
+        self.labelPath = labelPath
+        """Creates a graph from saved GraphDef file and returns a saver."""
+        # Creates graph from saved graph_def.pb.
+        with tf.gfile.FastGFile(modelPath, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
 
-    with open(file_path, 'rb') as image:
-        image_content = base64.b64encode(image.read())
-        service_request = service.images().annotate(body={
-            'requests': [{
-                'image': {
-                    'content': image_content.decode('UTF-8')
-                },
-                'features': [{
-                    'type': 'LABEL_DETECTION',
-                    'maxResults': 10
-                }]
-            }]
-        })
-        response = service_request.execute()
+        self.sess = tf.Session()
+        self.softmax_tensor = self.sess.graph.get_tensor_by_name('final_result:0')
 
-        labels = response['responses'][0].get('labelAnnotations', [])
-        return labels
+    def get_image_labels(self, imagePath):
+        answer = None
+
+        if not tf.gfile.Exists(imagePath):
+            tf.logging.fatal('File does not exist %s', imagePath)
+            return answer
+
+        image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
+
+        predictions = self.sess.run(self.softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+        top_k = predictions.argsort()[-5:][::-1] # Getting top 5 predictions
+        f = open(self.labelPath, 'rb')
+        lines = f.readlines()
+        labels = [str(w).replace("\n", "") for w in lines]
+
+        answers = []
+        for node_id in top_k:
+            human_string = labels[node_id]
+            score = predictions[node_id]
+            answers.append({"type": human_string, "score" : score })
+        return answers
+
+classifier = Classifier(os.path.abspath('classifier/trained_graph.pb'), os.path.abspath('classifier/output_labels.txt'))
+print classifier.get_image_labels('img/classificationImage.jpg')
+print classifier.get_image_labels('img/bottles/bottle0.jpg')
