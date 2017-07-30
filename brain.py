9d9c5fd8201b85ad4e4f398eb7b27fdf1b9f463f
patil215 <neilpatil215@gmail.com>
2017-07-29 17:57:31 -0700

    Add brain abstraction and tweak sorter

---
+++ brain.py
@@ -0,0 +1,2 @@
+def isTrash(labels):
+	return 'plastic' not in str(labels)
--- sorter.py
+++ sorter.py
@@ -2,32 +2,15 @@ import servo
 import ui
 import camera
 import vision
-
+import motiondetector
 import time 
 
 TRASH_POS = 40
 RECYCLE_POS = 200
 NEUTRAL_POS = 105
 
-def classify(imgpath):
-	labels = vision.get_image_labels(imgpath)
-	print labels
-	return 'plastic' in str(labels)
-	
-	#servo.move(20)
-	# if 'plastic' in str(labels):
-	# 	print "a bottle"
-	# 	servo.move(200)
-	# else:
-	# 	print "not a bottle"
-	# 	servo.move(40)
-	# servo.move(105)
-
-
-
-
 def sort_trash(imgpath):
-	ui.start_status_shower_thread()
+	statusThread = ui.start_status_shower_thread()
 
 	while True:
 		servo.move(NEUTRAL_POS)
@@ -35,26 +18,26 @@ def sort_trash(imgpath):
 
 		# wait for camera to detect motion, then sleep for a bit to
 		# let the object settle down
-		waitForMotionDetection()
-		time.sleep(2) # TODO maybe remove
+		motiondetector.waitForMotionDetection()
+		time.sleep(1) # Lets object settle down, TODO maybe remove
 
-		set_status("classifying")
+		ui.set_status("classifying")
 
 		# take a photo and classify it
 		camera.takePhoto(imgpath)
-		is_trash = classify(imgpath)
+		labels = vision.get_image_labels(imgpath)
+		is_trash = brain.isTrash(labels)
 
 		if is_trash:
 			print("It's trash.")
-			set_status("trash")
+			ui.set_status("trash")
 			servo.move(TRASH_POS)
 		else:
 			print("It's recyclable.")
-			set_status("recycling")
+			ui.set_status("recycling")
 			servo.move(RECYCLE_POS)
 
-
-
+	statusThread.shutdown()
 
 def main():
 	sort_trash('img/classificationImage.jpg')
