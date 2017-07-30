7b8136d9cf48650edc5ba13667f43f0302924066
Neil Patil <neilpatil215@gmail.com>
2017-07-30 10:08:50 +0000

    Merge in changes

--- databasehelper.py
+++ databasehelper.py
@@ -5,6 +5,7 @@ import time
 class Database:
 	firebase = None
 	version = 0.2
+	databaseName = "recyclingData"
 
 	def __init__(self):
 		self.firebase = firebase.FirebaseApplication("https://smart-garbage-4ba99.firebaseio.com/", None)
@@ -13,5 +14,5 @@ class Database:
 		timestamp = int(time.time()) # Seconds since UNIX epoch
 		image = open(imageFilePath, "rb")
 		imagebase64 = base64.b64encode(image.read())
-		data = {'image': imagebase64, 'labels': labels, 'isTrash' : isTrash, 'timestamp': timestamp, 'recyclingLabel': pickedRecyclingLabel, 'version': version}
-		self.firebase.post('/trashLogs', data)
+		data = {'image': imagebase64, 'labels': labels, 'isTrash' : isTrash, 'timestamp': timestamp, 'recyclingLabel': pickedRecyclingLabel, 'version': self.version}
+		self.firebase.post(self.databaseName, data)
