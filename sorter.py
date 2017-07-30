8f1831b6f5c26cca4955dd8cf7253b2c634af2a9
Filip <filip_bystricky@brown.edu>
2017-07-29 17:54:05 -0700

    sorting loop

--- sorter.py
+++ sorter.py
@@ -3,21 +3,61 @@ import ui
 import camera
 import vision
 
+import time 
+
+TRASH_POS = 40
+RECYCLE_POS = 200
+NEUTRAL_POS = 105
+
 def classify(imgpath):
 	labels = vision.get_image_labels(imgpath)
 	print labels
+	return 'plastic' in str(labels)
 	
 	#servo.move(20)
-	if 'plastic' in str(labels):
-		print "a bottle"
-		servo.move(200)
-	else:
-		print "not a bottle"
-		servo.move(40)
-	servo.move(105)
+	# if 'plastic' in str(labels):
+	# 	print "a bottle"
+	# 	servo.move(200)
+	# else:
+	# 	print "not a bottle"
+	# 	servo.move(40)
+	# servo.move(105)
+
+
+
+
+def sort_trash(imgpath):
+	ui.start_status_shower_thread()
+
+	while True:
+		servo.move(NEUTRAL_POS)
+		set_status("ready")
+
+		# wait for camera to detect motion, then sleep for a bit to
+		# let the object settle down
+		waitForMotionDetection()
+		time.sleep(2) # TODO maybe remove
+
+		set_status("classifying")
+
+		# take a photo and classify it
+		camera.takePhoto(imgpath)
+		is_trash = classify(imgpath)
+
+		if is_trash:
+			print("It's trash.")
+			set_status("trash")
+			servo.move(TRASH_POS)
+		else:
+			print("It's recyclable.")
+			set_status("recycling")
+			servo.move(RECYCLE_POS)
+
+
+
+
 def main():
-	camera.takePhoto('img/classificationImage.jpg')
-	classify('img/classificationImage.jpg')
+	sort_trash('img/classificationImage.jpg')
 
 if __name__ == '__main__':
 	main()
--- ui.py
+++ ui.py
@@ -1,32 +1,78 @@
 import rainbowhat
 import time
+import threading
+
+status_lock = threading.Condition()
+status = "ready"
+
+# each color is a tuple (r, g, b)
+status_colors = {
+	"ready": (0, 1, 0),
+	"classifying": (0, 0, 1),
+	"recycling": (0, 1, 1),
+	"trash": (1, 0, 1),
+}
+
 
 def display_message(message):
     rainbowhat.display.print_str(message)
     rainbowhat.display.show()
 
 
-def display_long_message(message, n = 1, delay=.25):
-	for i in range(n):
-		for j in range(len(message)):
-			time.sleep(delay)
-			display_message(message[j : min(j + 4, len(message))])
-		display_message("    ") # clear the display
-		if i < (n - 1):
-			time.sleep(1) # sleep after each loop except the last 
+
+def display_long_message(message, delay=.25):
+	for i in range(len(message)):
+		time.sleep(delay)
+		display_message(message[i : min(i + 4, len(message))])
+	display_message("    ") # clear the display
+
 
 def set_all_pixels(*args, **kwargs):
 	for pixel in range(7):
 		rainbowhat.rainbow.set_pixel(pixel, *args, **kwargs)
 	rainbowhat.rainbow.show()
 
-def show_decision(is_trash):
-	if is_trash:
-		set_all_pixels(1, 0, 0)
-		display_long_message("TRASH")
+
+def show_status(status):
+	if status == "ready":
+		set_all_pixels(0, 1, 0, brightness=1)
 	else:
-		set_all_pixels(0, 1, 0)
-		display_long_message("RECYCLING")
+		set_all_pixels(1, 0, 0, brightness=1)
+	display_long_message(status)
+
+# def show_decision(is_trash):
+# 	if is_trash:
+# 		set_all_pixels(1, 0, 0)
+# 		display_long_message("TRASH")
+# 	else:
+# 		set_all_pixels(0, 1, 0)
+# 		display_long_message("RECYCLING")
+
+
+class StatusShower(threading.Thread):
+	def run(self):
+		global status
+		while True:
+			status_lock.acquire()
+			s = status
+			status_lock.release()
+
+			color = status_colors[s]
+			set_all_pixels(*color, brightness=1)
+			display_long_message(s)
+
+			time.sleep(1)
+
+def start_status_shower_thread():
+	StatusShower()
+
+def set_status(s):
+	global status 
+	status_lock.acquire()
+	status = s
+	status_lock.release()
+
+
 
 @rainbowhat.touch.A.press()
 def press_a(channel):
